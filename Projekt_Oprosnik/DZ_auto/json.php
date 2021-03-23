62fa7ac5f403221e81fa42ebec07774b6f211fe0
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-03-23 23:17:46 +0300

    change index1, add file main3 and opros.json

---
+++ Projekt_Oprosnik/DZ_auto/json.php
@@ -0,0 +1,9 @@
+<?php
+$answer1=$_POST["1"];
+$answer2=$_POST["2"];
+$answer3=$_POST["3"];
+$answer4=$_POST["4"];
+$data = json_decode(file_get_contents("opros.json"));
+$data[] = ["msg1" => $answer1, "msg2" => $answer2, "msg3" => $answer3,"msg4" => $answer4];
+file_put_contents("opros.json", json_encode($data));
+header("location:index1.php");
